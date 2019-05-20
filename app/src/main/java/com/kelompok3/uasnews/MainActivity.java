97bdf47517cc9f9501f016ca631e21cc280fcc96
bailram <dhimasbayu.9f@gmal.com>
2019-05-20 08:35:54 +0700

    first commit

---
+++ app/src/main/java/com/kelompok3/uasnews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kelompok3.uasnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
